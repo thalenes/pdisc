f953090cf74742d56776469dc2801e2920fb1bb8
Thalene <153958765+thalenes@users.noreply.github.com>
2024-04-20 01:29:32 +0200

    Add files via upload

---
+++ EjerciciosJavaScript/Ejercicio10.js
@@ -0,0 +1,18 @@
+let num = 5;
+let num1 = 8;
+
+if (num < num1){
+    console.log("No es mayor");
+}
+
+if (num1 > 0){
+    console.log("Es positivo");
+}
+
+if (num != 0){
+    console.log("Es distinto de 0");
+}
+
+if (num1 + 1 < num){
+    console.log("Incrementar unidad en un valor, por ende mayor que num");
+}
